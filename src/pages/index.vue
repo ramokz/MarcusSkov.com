752d29a883aedb32aaef0c63608a5946f960975d
Marcus <Hello@MarcusSkov.com>
2022-03-10 14:04:15 +0000

    Disabled header and added WIP message

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -26,22 +26,25 @@ const projectListFade = () => {
 
 <template>
   <div ref="projectList">
-    <div
-      class="
-        flex
-        flex-col
-        items-center
-        justify-center
-        h-screen-1/2
-        shadow-xl"
-    >
-      <h1 class="h1">
-        Hi
-      </h1>
-      <h2 class="h4 mt-12">
-        I'm a UI Designer and Developer working in games
-      </h2>
+    <div class="opacity-20 fixed top-12 right-12">
+      Site still work-in-progress
     </div>
+    <!--    <div-->
+    <!--      class="-->
+    <!--        flex-->
+    <!--        flex-col-->
+    <!--        items-center-->
+    <!--        justify-center-->
+    <!--        h-screen-1/2-->
+    <!--        shadow-xl"-->
+    <!--    >-->
+    <!--      <h1 class="h1">-->
+    <!--        Hi-->
+    <!--      </h1>-->
+    <!--      <h2 class="h4 mt-12">-->
+    <!--        I'm a UI Designer and Developer working in games-->
+    <!--      </h2>-->
+    <!--    </div>-->
     <ProjectListHeader
       v-for="(story, index) in storyStore.projectData"
       :key="story.id"
