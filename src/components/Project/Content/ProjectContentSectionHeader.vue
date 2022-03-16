6e0f866e8b71ec0459c2df2b88b924a747f9399a
Marcus <hello@marcusskov.com>
2022-03-16 17:48:33 +0000

    Removed typo

--- src/components/Project/Content/ProjectContentSectionHeader.vue
+++ src/components/Project/Content/ProjectContentSectionHeader.vue
@@ -18,7 +18,7 @@ const richTextComputed = computed(() => {
 
 
 <template>
-  <div class="container text-center mx-auto not mb-12 mt-64">
+  <div class="container text-center mx-auto mb-12 mt-64">
     <h1 class="h2">{{ content.title }}</h1>
     <div class="divider w-1/2 mx-auto"/>
     <p class="body w-1/2 mx-auto" v-html="richTextComputed"/>
