230a9f2f5914f9be58dec6cc1c3c8729187af8cb
Marcus <hello@marcusskov.com>
2022-03-16 01:28:00 +0000

    Minor redesign of back button

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -3,27 +3,25 @@
 <template>
   <div class="sticky z-1 top-6 container mx-auto h-0">
     <div
-      class="inline-block cursor-pointer"
+      class="inline-block cursor-pointer select-none"
       @click="$router.go(-1)"
     >
       <div
         class="
           flex
           items-center
-          py-1
+          py-2
           px-2
-          bg-light
+          bg-dark
           hover:bg-dark
-          border-2
-          border-dark
+          border
+          border-core
           hover:border-light
-          rounded-md
-          stroke-dark
+          stroke-core
           hover:stroke-light
-          space-x-1
-          text-xl
-          text-dark
-          hover:text-light"
+          text-lg
+          text-light
+          uppercase"
       >
         <svg
           width="24"
@@ -32,14 +30,11 @@
         >
           <path
             d="M14.9865 7L6 16.5M6 16.5L14.9865 26M6 16.5H26.8108"
-            stroke-width="5"
+            stroke-width="4"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
         </svg>
-        <div class="">
-          Back
-        </div>
       </div>
     </div>
   </div>
