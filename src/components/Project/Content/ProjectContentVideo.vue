345793e3f322e1f4ec775465e468485f2021854d
Marcus <hello@marcusskov.com>
2022-03-17 23:23:27 +0000

    Made videos require user input before playing

--- src/components/Project/Content/ProjectContentVideo.vue
+++ src/components/Project/Content/ProjectContentVideo.vue
@@ -8,36 +8,15 @@ const props = defineProps({
   content: {
     type: Object,
     required: true
-  } })
-
-const videoOverlay = ref()
+  }
+})
 const video = ref<HTMLVideoElement>()
-const videoPlay = ref<HTMLElement>()
-
 let options = {
   root: null,
   rootMargin: '0px',
   threshold: 0.7
 }
-
-// const videoOverlayComputed = computed(() => {
-//   const videoWidth = `${video.value?.offsetWidth.toString()}px`
-//   const videoHeight = `${video.value?.offsetHeight.toString()}px`
-//
-//   return {width: videoWidth, height: videoHeight}
-// })
-
-gsap.registerEffect({
-  name: 'videoFade',
-  effect: (targets: HTMLElement, config: { opacity: number }) => {
-    return gsap.to(targets, {
-      duration: 0.4,
-      opacity: config.opacity
-    })
-  }
-})
-
-
+let pauseOverlay = ref(false)
 
 onMounted(() => {
 
@@ -46,84 +25,45 @@ onMounted(() => {
 
       entries.forEach(entry=>{
 
-        if (entry.isIntersecting) {
-          // console.log('Playing')
+        if (!entry.isIntersecting) {
 
-          // gsap.to(video.value, {
-          //   opacity: 1,
-          //   duration: 0.4
-          // })
-
-          gsap.effects.videoFade(video.value, { opacity: 1 })
-
-          target.play()
-        }
-        else {
-          // console.log('Pausing')
-
-          gsap.effects.videoFade(video.value, { opacity: 0.2 })
-
-          // gsap.to(video.value, {
-          //     opacity: 0.2,
-          //     duration: 0.4
-          //   })
+          pauseOverlay.value = false
+          video.value?.pause()
 
           target.pause()
         }
       })
     }, {
-      threshold: 0.9
+      threshold: 0.6
     })
 
     io.observe(target)
   }
-  videoObserver(video.value)
 
+  videoObserver(video.value as HTMLVideoElement)
 
 })
 
-const playBtnDur = 0.12
-const overlayOpacity = 0.8
-const asset = null
-const trigger = null
+const playVideo = () => {
+  pauseOverlay.value = !pauseOverlay.value
+  console.log(pauseOverlay.value)
 
-// const playBtn = (shouldPlay : boolean = false, shouldPause : boolean = false) => {
-//   if (!video.value) return
-//   if (video.value.paused && !shouldPause || shouldPlay) {
-//
-//     const playbackPromise = video.value.play()
-//     if (playbackPromise !== undefined) {
-//       playbackPromise.then(_ => {
-//         gsap.set([videoOverlay.value, videoPlay.value], {
-//           // opacity: 0,
-//           visibility: 'hidden',
-//           pointerEvents: 'inherit'
-//         })
-//         video.value.controls = true
-//         videoOverlay.value.style.opacity = '0';
-//         videoPlay.value.style.opacity = '0'
-//       }).catch(err => {
-//         console.log(err)
-//       })
-//     }
-//   } else {
-//     video.value.pause();
-//     gsap.set(videoOverlay.value, {
-//       opacity: overlayOpacity,
-//       visibility: 'visible',
-//       pointerEvents: 'none'
-//     })
-//     video.value.controls = false
-//     // gsap.set(this.$refs.videoPlay, {
-//     //   opacity: 1,
-//     //   visibility: 'visible'
-//     // })
-//   }
-// }
+  if (!pauseOverlay.value) {
+    video.value?.play()
+  }
+  else {
+    video.value?.pause()
+  }
+}
+
+
 </script>
 
 <template>
-  <div ref="mediaContainer">
+  <div
+    ref="mediaContainer"
+    class="relative"
+  >
     <div
       class="relative mx-auto"
     />
@@ -131,20 +71,38 @@ const trigger = null
     <video
       :id="props.content.asset.filename"
       ref="video"
-      class="flex max-h-screen-md mx-auto max-w-full"
+      class="flex max-h-screen-md mx-auto max-w-full cursor-pointer"
       loop
       muted
       controls
+      @click="playVideo"
     >
       <source
         :src="props.content.asset.filename"
         type="video/mp4"
       >
     </video>
-    <!-- //.video-overlay(
-	//  v-show="videoOverlayVisible"
-	//  class="absolute opacity-80 top-0 bg-dark w-full h-full")
-	//h2(class="text-white") {{videoOverlayComputed}} -->
-  <!-- //src="~/assets/play.svg" -->
+    <div v-show="!pauseOverlay">
+      <div
+        class="absolute opacity-80 top-0 bg-dark w-full h-full pointer-events-none"
+      />
+      <svg
+        width="64"
+        height="64"
+        viewBox="0 0 32 32"
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none"
+      >
+        <circle
+          cx="16"
+          cy="16"
+          r="14.5"
+          class="stroke-2 stroke-core"
+        />
+        <path
+          d="M23.7876 16.1416L11.8938 23.0085L11.8938 9.27469L23.7876 16.1416Z"
+          class="stroke-core stroke-1"
+        />
+      </svg>
+    </div>
   </div>
 </template>
