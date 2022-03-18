5ae8ffdc6c689614bb23b32a9d275bcac82711f2
Marcus <hello@marcusskov.com>
2022-03-18 01:11:43 +0000

    Updated design of 404.vue

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,15 +1,18 @@
 <template>
-<div class="text-2xl">
-  Page not found
-</div>
+  <div class="flex flex-col w-screen h-screen items-center justify-center">
+    <h2 class="h2 uppercase">
+      Page not found
+    </h2>
+    <div class="divider" />
+    <router-link
+      to="/"
+      class="no-underline"
+    >
+      <div class="border border-core p-4">
+        <h3 class="h3 text-core hover:text-core">
+          Return to home page
+        </h3>
+      </div>
+    </router-link>
+  </div>
 </template>
-
-<script>
-export default {
-  name: "404"
-}
-</script>
-
-<style scoped>
-
-</style>
